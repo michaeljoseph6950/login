868c5ac7072f09d112f461d869a79869f6f5a62c
Michael <michaeljoseph6950@gmail.com>
2022-09-18 00:52:13 +0300

    ch

--- public/js/google/broconfirm.js
+++ public/js/google/broconfirm.js
@@ -30,6 +30,10 @@ const signInWithPhoneButton = document.getElementById('signInWithPhone');
 const getCodeButton = document.getElementById('getCode');
 
 
+const signGoogle = document.getElementById("signGoogle");
+const signYahoo = document.getElementById('signYahoo');
+
+
 const auth = firebase.auth();
 
 
@@ -144,6 +148,30 @@ if (auth.isSignInWithEmailLink(window.location.href)) {
 }
 
 
+
+const signInWithGoogle = () => {
+	const googleProvider = new firebase.auth.GoogleAuthProvider;
+	auth.signInWithPopup(googleProvider).then(() => {
+		sendVerificationEmail();
+		window.location.reload();
+	}).catch(error => {
+		alert(error.message)
+	});
+};
+signGoogle.addEventListener("click", signInWithGoogle);
+
+
+
+const signInWithYahoo = () => {
+	const yahooProvider = new firebase.auth.OAuthProvider('yahoo.com');
+	auth.signInWithPopup(yahooProvider).then(() => {
+		sendVerificationEmail();
+		window.location.reload();
+	}).catch(error => {
+		alert(error.message);
+	})
+}
+signYahoo.addEventListener("click", signInWithYahoo);
 
 
 
