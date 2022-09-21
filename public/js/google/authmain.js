f5ca67e9e592fb1c0f9ad69bf2c4d677034928e0
Michael <michaeljoseph6950@gmail.com>
2022-09-21 22:21:12 +0300

    ch

--- public/js/google/authmain.js
+++ public/js/google/authmain.js
@@ -50,6 +50,27 @@ auth.onAuthStateChanged(user => {
 	}
 });
 
+const logOut = document.getElementById('logout');
+logOut.addEventListener('click', () => {
+    if(auth.currentUser.isAnonymous) {
+		auth.currentUser.delete()
+			.then(() => {
+				window.location.assign('index');
+			})
+			.catch(error => {
+				console.error(error);
+			})
+	} else {
+		auth.signOut()
+			.then(() => {
+				window.location.assign('index');
+			})
+			.catch(error => {
+				console.error(error);
+			})
+	}
+})
+
 
 jinaHolder.addEventListener("change", () => {
 	auth.currentUser.updateProfile({
